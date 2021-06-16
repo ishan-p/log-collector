5475d0e479b72beb2ab5b25671bff21c5b0bc86a
Ishan <ishan@one2n.in>
2021-06-16 17:17:43 +0530

    Add function to read client config file in json format

--- config_manager.go
+++ config_manager.go
@@ -1,5 +1,12 @@
 package logcollector
 
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"os"
+)
+
 type Config struct {
 	Watchers    []Watcher          `json:"watchers"`
 	Collector   ServerConnection   `json:"server"`
@@ -10,5 +17,32 @@ func initiateConfigWithDefaults() Config {
 	var config Config
 	config.Collector = defaultServerConnection()
 	config.RetryParams = defaultRetryConfiguration()
+	return config
+}
+
+func validateConfig(config Config) (bool, error) {
+	if len(config.Watchers) < 1 {
+		return false, errors.New("Cannot initiate agent with 0 watchers")
+	}
+	// TODO: Add more validations around different values
+	return true, nil
+}
+
+func readClientConfigJSON(configFilePath string) Config {
+	data, err := os.ReadFile(configFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	isValidJson := json.Valid(data)
+	if !isValidJson {
+		log.Fatal("Provided config file is an invalid JSON")
+	}
+
+	config := initiateConfigWithDefaults()
+	_ = json.Unmarshal(data, &config)
+	isValid, validationError := validateConfig(config)
+	if !isValid {
+		log.Fatal(validationError)
+	}
 	return config
 }
