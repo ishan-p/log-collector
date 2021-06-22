1acacf5e8e018b41c26baf15d80ae30426ec7d19
Ishan <ishan@one2n.in>
2021-06-22 11:41:46 +0530

    Add basic client server TCP implementation
    Client initiates server connection on every even notification and sends
    the record for storing.
    Server handles every connection in a separate go routine processing the
    specified commands.

--- client.go
+++ client.go
@@ -1,7 +1,11 @@
 package logcollector
 
 import (
+	"encoding/json"
 	"log"
+	"net"
+	"strconv"
+	"time"
 )
 
 func RunAgent(configFile string) {
@@ -12,13 +16,59 @@ func RunAgent(configFile string) {
 		notificationChannel := make(chan string)
 		defer close(notificationChannel)
 		go watch(watcher.FileName, notificationChannel)
-		go notify(notificationChannel)
+		go notify(notificationChannel, config.Collector, watcher.Destination, watcher.Tags, config.RetryParams)
 	}
 	<-done
 }
 
-func notify(ch chan string) {
+func notify(ch chan string, server ServerConnection, destination string, tags []map[string]string, retryConfig RetryConfiguration) {
 	for notification := range ch {
-		log.Println(notification)
+		sendToServer(notification, server, destination, tags, retryConfig)
 	}
+}
+
+func sendToServer(notification string, server ServerConnection, destination string, tags []map[string]string, retryConfig RetryConfiguration) {
+	dest := server.Host + ":" + strconv.Itoa(server.Port)
+	conn, err := net.Dial("tcp", dest)
+	if err != nil {
+		log.Println("Could not connect to the server")
+	}
+	if conn == nil {
+		log.Println("Failed to create successful connection")
+	}
+	defer conn.Close()
+
+	commandResponse, err := initCollectRequest(conn)
+	if err != nil || !commandResponse.Begin {
+		log.Println("Failed to initiate command")
+	}
+
+	sendLog(conn, notification, destination, tags)
+}
+
+func initCollectRequest(conn net.Conn) (CommandResponse, error) {
+	var commandResponse CommandResponse
+	commandRequest := CommandRequest{
+		Command: "collect",
+	}
+	if err := json.NewEncoder(conn).Encode(&commandRequest); err != nil {
+		log.Println(err)
+	}
+	err := json.NewDecoder(conn).Decode(&commandResponse)
+	return commandResponse, err
+}
+
+func sendLog(conn net.Conn, notification string, destination string, tags []map[string]string) {
+	collectCmdPayload := CollectCmdPayload{
+		Record:      notification,
+		Timestamp:   time.Now().Unix(),
+		Tags:        tags,
+		Destination: destination,
+	}
+	if err := json.NewEncoder(conn).Encode(&collectCmdPayload); err != nil {
+		log.Println(err)
+	}
+	var collectResp CollectCmdResponse
+	json.NewDecoder(conn).Decode(&collectResp)
+	log.Println(collectResp)
 }
--- server.go
+++ server.go
@@ -1,9 +1,37 @@
 package logcollector
 
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net"
+	"strconv"
+)
+
 type ServerConnection struct {
 	Host              string `json:"host"`
 	Port              int    `json:"port"`
 	ServerWaitTimeSec int    `json:"wait_time"`
+}
+
+type CollectCmdPayload struct {
+	Timestamp   int64               `json:"timestamp"`
+	Tags        []map[string]string `json:"tags"`
+	Record      string              `json:"record"`
+	Destination string              `json:"destination"`
+}
+
+type CollectCmdResponse struct {
+	Ack bool `json:"ack"`
+}
+
+type CommandRequest struct {
+	Command string `json:"cmd"`
+}
+
+type CommandResponse struct {
+	Command string `json:"cmd"`
+	Begin   bool   `json:"begin"`
 }
 
 func defaultServerConnection() ServerConnection {
@@ -12,4 +40,92 @@ func defaultServerConnection() ServerConnection {
 	connection.Port = 8888
 	connection.ServerWaitTimeSec = 3
 	return connection
+}
+
+func RunServer() {
+	port := 8888
+	src := ":" + strconv.Itoa(port)
+	listener, err := net.Listen("tcp", src)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("Listening on ", src)
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Connection error: ", err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Println("error closing connection:", err)
+		}
+	}()
+
+	for {
+		command, err := waitForCommand(conn)
+		if err != nil {
+			if err == io.EOF {
+				log.Println("Closing connection")
+				break
+			}
+			log.Println(err)
+		}
+		beginTransaction(command.Command, conn)
+		handleRequest(command.Command, conn)
+	}
+}
+
+func waitForCommand(conn net.Conn) (CommandRequest, error) {
+	decoder := json.NewDecoder(conn)
+	var request CommandRequest
+	if err := decoder.Decode(&request); err != nil {
+		return request, err
+	}
+	return request, nil
+}
+
+func beginTransaction(command string, conn net.Conn) {
+	initRequest := CommandResponse{
+		Command: command,
+		Begin:   true,
+	}
+	encoder := json.NewEncoder(conn)
+	if err := encoder.Encode(&initRequest); err != nil {
+		log.Println(err)
+	}
+}
+
+func handleRequest(activeCommand string, conn net.Conn) {
+	switch activeCommand {
+	case "collect":
+		handleCollect(conn)
+	default:
+		log.Println("Unrecognized command")
+	}
+}
+
+func handleCollect(conn net.Conn) {
+	decoder := json.NewDecoder(conn)
+	var collectPayload CollectCmdPayload
+	if err := decoder.Decode(&collectPayload); err != nil {
+		log.Println(err)
+	}
+	ack := collect(collectPayload)
+	collectResponse := CollectCmdResponse{Ack: ack}
+	encoder := json.NewEncoder(conn)
+	if err := encoder.Encode(&collectResponse); err != nil {
+		log.Println(err)
+	}
+}
+
+func collect(record CollectCmdPayload) bool {
+	log.Println(record)
+	return true
 }
